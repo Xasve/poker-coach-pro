c814f31cb4285edb1aa7ffa89bf6aa0195d1bd8e
Xasve <159001026+Xasve@users.noreply.github.com>
2025-12-18 19:24:35 -0600

    wdadawdwad

--- poker_coach_pro.py
+++
@@ -1,6 +0,0 @@
-﻿#!/usr/bin/env python3
-print('POKER COACH PRO - Script principal')
-print('=' * 40)
-print('Ejecuta: python poker_coach_pro.py')
-print('El sistema esta listo para usar.')
-input('Presiona Enter para salir...')
